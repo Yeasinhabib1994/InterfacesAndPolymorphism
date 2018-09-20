0b578fceaf902c5e40576dbe90540f86a3da472b
yeasin habib <yeasinhabib1994@gmail.com>
2018-09-20 22:31:34 +0600

    Add project files.

---
+++ InterfacesAndPolymorphism/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesAndPolymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var encoder = new VideoEncoder();
+            encoder.RegisterNotificationChannel(new MailNotificationChannel());
+            encoder.RegisterNotificationChannel(new SmsNotificationChannel());
+            encoder.Encode(new Video());
+        }
+    }
+
+    public class VideoEncoder
+    {
+        private readonly IList<INotificationChannel> _notificationChannels;
+
+        public VideoEncoder()
+        {
+            _notificationChannels = new List<INotificationChannel>();
+        }
+
+        public void Encode (Video video)
+        {
+            foreach (var channel in _notificationChannels)
+                channel.Send(new Message());
+        } 
+
+        public void RegisterNotificationChannel(INotificationChannel channel)
+        {
+            _notificationChannels.Add(channel);
+        }
+    }
+
+    public interface INotificationChannel
+    {
+        void Send(Message message);
+    }
+
+    public class MailNotificationChannel : INotificationChannel
+    {
+        public void Send(Message message)
+        {
+            Console.WriteLine("sending mail....");
+        }
+    }
+
+    public class SmsNotificationChannel : INotificationChannel
+    {
+        public void Send(Message message)
+        {
+            Console.WriteLine("sending sms....");
+        }
+    }
+
+    public class Message
+    {
+    }
+
+    public class Video
+    {
+
+    }
+}
